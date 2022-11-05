00e22765a805a1f6f36d5ced369ff02e0efdcc69
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-11-05 19:10:42 +0330

    complete website

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="about-container">
+      <h3>Welcome to Magiciant Calculator!</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec mauris bibendum mollis.
+        Vivamus quis fermentum urna, at scelerisque nunc. Sed sit amet ante nisi.
+        Mauris a sapien vitae metus vulputate efficitur.
+        Donec viverra erat sed porta ultricies. Donec at turpis velit. Nulla et vulputate nisi.
+        Vestibulum in efficitur leo, sit amet placerat nibh. Fusce in ligula massa.
+        Vestibulum nulla orci, pretium a velit in, egestas sollicitudin odio.
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec mauris bibendum mollis.
+        Vivamus quis fermentum urna, at scelerisque nunc. Sed sit amet ante nisi.
+        Mauris a sapien vitae metus vulputate efficitur.
+        Donec viverra erat sed porta ultricies. Donec at turpis velit. Nulla et vulputate nisi.
+        Vestibulum in efficitur leo, sit amet placerat nibh. Fusce in ligula massa.
+        Vestibulum nulla orci, pretium a velit in, egestas sollicitudin odio.
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        {' '}
+
+      </p>
+    </div>
+  );
+}
+
+export default Home;
